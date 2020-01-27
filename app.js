843dd88081eff8157b67c193ccdc9aba94f04f53
Sander <sander.randmets@ametikool.ee>
2020-01-27 11:34:00 +0200

    calculator

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const btns = Array.prototype.slice.call(document.querySelectorAll('.btn'))
-const answer = document.querySelector('#answer')
+const answer = document.querySelector('#answer');
+let currentNumber = '';
+let firstNumber;
 
 btns.map(btn => btn.addEventListener('click', (evt) => {
-    answer.innerText=evt.target.innerText
+    console.log(parseInt(evt.target.innerText))
+    if (! isNAN(parseInt(evt.target.innerText)) >= 0) {
+        saveCurrentNumber(evt.target.innerText);
+    }
+    if () {
+        activateOperator(evt.target.innerText)
+    }
 }));
+
+function saveCurrentNumber(number){
+    currentNumber += number
+    updatedisplay();
+}
+
+function updatedisplay(){
+    answer.innerText = currentNumber
+}
+
+function activateOperator(evt){
+    evt.target.innerText
+}
+    
+/*answer.innerText=evt.target.innerText*/
